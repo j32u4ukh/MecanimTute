f2b0a40e92e57f34616fde069ba6c38c574fd736
j32u4ukh <j32u4ukh@gmail.com>
2020-07-15 16:40:48 +0800

    載入動畫

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public AnimatorStateInfo info;
+    static int idle_state = Animator.StringToHash("Base Layer.Idle");
+    static int run_state = Animator.StringToHash("Base Layer.Run");
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("is_run", false);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("is_run", true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MecanimTute
 阿葉的 Animator 教學
+
+## 調高模型亮度
+
+![原教學-調高模型亮度](repo/light_probes.png)
+
+原教學透過點選 Use Light Probes 來使得黯淡的模型變得明亮，但新的編輯器已無該選項，因此改透過修改 Robot_Color 的 Shader，選擇有 
+Self-Illumin 的 Shader 可以達到相同效果。
+
+[參考網站](https://blog.csdn.net/w742874584/article/details/50650191)
